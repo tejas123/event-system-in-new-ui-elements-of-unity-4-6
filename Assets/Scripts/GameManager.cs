50c77c8e7237aa685d718adacba53aa7cb6c41fe
Bharat <bharatappguru@gmail.com>
2015-04-17 16:11:00 +0530

    Event System in New UI Elements of Unity 4.6 Tutorial

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+		public void Play ()
+		{
+				Application.LoadLevel (Application.loadedLevel + 1);
+		}
+}
